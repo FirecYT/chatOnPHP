278cf13686e13a2238b45899f5e6b484c115c02a
FirecYT <kratos150@yandex.ru>
2019-01-15 16:41:31 +0300

    GOOD!!!

--- server.php
+++ server.php
@@ -1,7 +1,13 @@
 <?php
-if($_GET['text']){
-    file_put_contents("chat.txt", date('[Y-m-d]-[H:i:s] ').$_GET['text']."\n", FILE_APPEND | LOCK_EX);
+setlocale(LC_ALL, 'ru-RU'); // Language
+date_default_timezone_set("Europe/Moscow"); // Tinezone
+$file = "data.html"; // Name file for data
+$arg = "text"; // Arg in URL
+$string = date('[Y-m-d]-[H:i:s] ').$_GET[$arg]."<br>\n"; // Convert string
+if($_GET[$arg]){
+	file_put_contents("data.html", $string, FILE_APPEND | LOCK_EX); // Put string with data in file
+	print_r(htmlspecialchars($_GET[$arg])); // Print what put in data
 } else {
-    print("Error");
+	print("0x000A");
 }
 ?>
