0e6ee6ec584b2c0faec54d019fb5d3656f1c6230
FirecYT <kratos150@yandex.ru>
2019-01-22 16:12:41 +0300

    Just edit

--- server.php
+++ server.php
@@ -1,12 +1,14 @@
 <?php
-setlocale(LC_ALL, 'ru-RU'); // Language
-date_default_timezone_set("Europe/Moscow"); // Tinezone
-$file = "data.html"; // Name file for data
-$arg = "text"; // Arg in URL
-$string = date('[Y-m-d]-[H:i:s] ').$_GET[$arg]."<br>\n"; // Convert string
 if($_GET[$arg]){
-	file_put_contents("data.html", $string, FILE_APPEND | LOCK_EX); // Put string with data in file
-	print_r(htmlspecialchars($_GET[$arg])); // Print what put in data
+	setlocale(LC_ALL, 'ru-RU'); // Установка языка
+	date_default_timezone_set("Europe/Moscow"); // и временной зоны
+	// Переменные
+	$file = "data.html"; // Имя файла даты
+	$arg = "text"; // Название аргумента
+	$string = date('[Y-m-d]-[H:i:s] ').$_GET[$arg]."<br>\n"; // Что будет записанно в дату
+	// Основные функции
+	file_put_contents("data.html", $string, FILE_APPEND | LOCK_EX); // Записываем строку в дату
+	print_r(htmlspecialchars($_GET[$arg])); // Что было записанно в дату
 } else {
 	print("0x000A");
 }
